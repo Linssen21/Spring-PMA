bbe0ebeea4616c64fcc9ba9124efaff35b1c7dc7
Linssen Diocares <32734222+Linssen21@users.noreply.github.com>
2022-05-22 22:37:36 +0800

    Project Status

--- project-management/src/main/java/com/sbtutorial/pma/controllers/HomeController.java
+++ project-management/src/main/java/com/sbtutorial/pma/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package com.sbtutorial.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbtutorial.pma.dao.EmployeeRepository;
 import com.sbtutorial.pma.dao.ProjectRepository;
+import com.sbtutorial.pma.dto.ChartData;
 import com.sbtutorial.pma.dto.EmployeeProject;
 import com.sbtutorial.pma.entities.Employee;
 import com.sbtutorial.pma.entities.Project;
@@ -26,13 +31,22 @@ public class HomeController {
 	EmployeeRepository employeeRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
-	
+	public String displayHome(Model model) throws JsonProcessingException {
+		Map<String, Object> map = new HashMap<>();
+		
 		 // Overrides the findAll to return a list of Project
 		List<Project> projects = proRepo.findAll();
 		 //Pass the data projectsList
 		model.addAttribute("projectsList", projects);
 		
+		// Get Project Status
+		List<ChartData> projectData = proRepo.getProjectStatus();
+		// Convert projectData Object into a JSON structure for Chart JS
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		// Output : [ ["NOTSTARTED", 1], ["INPROGRESS", 2], ["COMPLETED", 1] ]
+		model.addAttribute("projectStatusCount", jsonString);
+		 
 		 // Overrides the findAll to return a list of Project
 //		List<Employee> employees = employeeRepo.findAll();
 //		model.addAttribute("employeesList", employees);
@@ -40,6 +54,7 @@ public class HomeController {
 		// Execute the Custom Query from the (DAO) Data Access Object
 		List<EmployeeProject> employeesProjectCount = employeeRepo.employeeProject();
 		model.addAttribute("employeesProjectCount", employeesProjectCount);
+		
 		
 		// Render the home.html
 		return "main/home";
--- project-management/src/main/java/com/sbtutorial/pma/dao/ProjectRepository.java
+++ project-management/src/main/java/com/sbtutorial/pma/dao/ProjectRepository.java
@@ -2,8 +2,10 @@ package com.sbtutorial.pma.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.sbtutorial.pma.dto.ChartData;
 import com.sbtutorial.pma.entities.Project;
 
 /**
@@ -18,4 +20,7 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 	 */
 	@Override
 	public List<Project> findAll();
+	
+	@Query(nativeQuery = true, value = "SELECT  stage as label, COUNT(*) as value FROM PROJECT GROUP BY stage;")
+	public List<ChartData> getProjectStatus();
 }
