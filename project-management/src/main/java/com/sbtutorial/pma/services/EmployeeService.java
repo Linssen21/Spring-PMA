b31dbee90f13b80eec121efc837f331c9374f920
Linssen Diocares <32734222+Linssen21@users.noreply.github.com>
2022-05-23 20:23:53 +0800

    Primary and Qualifier Annotation

--- project-management/src/main/java/com/sbtutorial/pma/services/EmployeeService.java
+++ project-management/src/main/java/com/sbtutorial/pma/services/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.sbtutorial.pma.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.sbtutorial.pma.dao.EmployeeRepository;
@@ -11,15 +12,17 @@ public class EmployeeService {
 	/**
 	 * Field Injection
 	 */
+	@Qualifier("staffRepositoryImpl2")
 	@Autowired
-	EmployeeRepository employeeRepo;
+	IStaffRepository employeeRepo;
 
 	/**
 	 * Constructor Injection
 	 * @param employeeRepo
+	 * @Qualifier Annotation Select the Bean to be implemented, must be in camel case
 	 */
-//	public EmployeeService(EmployeeRepository employeeRepo) {
-//		super();
+//	public EmployeeService(@Qualifier("staffRepositoryImpl2") IStaffRepository employeeRepo) {
+//	super();
 //		this.employeeRepo = employeeRepo;
 //	}
 	
@@ -29,6 +32,7 @@ public class EmployeeService {
 	 * @param employeeRepo
 	 */
 //	@Autowired
+//	@Qualifier("staffRepositoryImpl2")
 //	public void setEmployeeRepo(EmployeeRepository employeeRepo) {
 //		this.employeeRepo = employeeRepo;
 //	}
